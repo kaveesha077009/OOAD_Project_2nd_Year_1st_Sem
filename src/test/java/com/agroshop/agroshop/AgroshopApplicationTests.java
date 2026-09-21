f40709d8f10f1af71e862145102476db1620befc
kaveesha077009 <kaveesharashmika0045@gmail.com>
2026-09-21 14:24:12 +0530

    Added Spring Boot base project setup

---
+++ src/test/java/com/agroshop/agroshop/AgroshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.agroshop.agroshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgroshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
